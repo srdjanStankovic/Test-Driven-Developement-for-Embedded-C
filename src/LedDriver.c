67c09fa21f10061bf4661be8c0282907269b3e78
srdjanStankovic <srdjanrstankovic@gmail.com>
2020-03-21 19:41:19 +0100

    Isolate led number into f

--- src/LedDriver.c
+++ src/LedDriver.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include "LedDriver.h"
 #include "../include/util/RuntimeError.h"
 
@@ -5,6 +6,12 @@ enum
 {
 	 ALL_LEDS_ON = ~0,
 	 ALL_LEDS_OFF = ~ALL_LEDS_ON
+};
+
+enum
+{
+	FIRST_LED = 1,
+	LAST_LED = 16
 };
 
 static uint16_t * ledsAddress;
@@ -18,6 +25,21 @@ static void updateHardware(void)
 static uint16_t convretLedNumberToBit(int ledNumber)
 {
 	return (1 << (ledNumber -1) );
+}
+
+static _Bool IsLedOutOfBounds(int ledNumber)
+{
+	return (ledNumber<FIRST_LED) || (ledNumber>LAST_LED);
+}
+
+static void setLedImageBit(int ledNumber)
+{
+	ledsImage |= convretLedNumberToBit(ledNumber);
+}
+
+static void clearLedImageBit(int ledNumber)
+{
+	ledsImage &= ~convretLedNumberToBit(ledNumber);
 }
 
 void LedDriver_Create(uint16_t * address)
@@ -34,26 +56,25 @@ void LedDriver_Destroy(void)
 
 void LedDriver_TurnOn(int ledNumber)
 {
-	if(ledNumber<=0 || ledNumber>16)
+	if(IsLedOutOfBounds(ledNumber))
 	{
 		RUNTIME_ERROR("LED Driver: out-of-bound LED", ledNumber);
 		return;
 	}
 
-	ledsImage |= convretLedNumberToBit(ledNumber);
+	setLedImageBit(ledNumber);
 	updateHardware();
 }
 
 void LedDriver_TurnOff(int ledNumber)
 {
-
-	if(ledNumber<=0 || ledNumber>16)
+	if(IsLedOutOfBounds(ledNumber))
 	{
 		RUNTIME_ERROR("LED Driver: out-of-bound LED", ledNumber);
 		return;
 	}
 
-	ledsImage &= ~convretLedNumberToBit(ledNumber);
+	clearLedImageBit(ledNumber);
 	updateHardware();
 }
 
